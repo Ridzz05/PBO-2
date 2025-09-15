fc2302d738c3153b558a133663063136ab37fa5d
M. Rizki A <muhrizkialghipari@gmail.com>
2025-09-15 09:29:29 +0700

    Create tugas1.java

---
+++ tugas1.java
@@ -0,0 +1,63 @@
+
+package javaapplication1;
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+//Variabel adalah : wadah yang menyimpan 1 niali, 1 tipe data, Dan nilainya juga bisa berubah
+//Penamaan identifier : 
+// 1. Case sensitive : huruf besar dan huruf kecil
+// 2. Tidak boleh ada spasi
+// 3. Tidak boleh mengandung operator / simbol
+// 4. Tidak boleh berupa keyword java : if, for, else 
+// 5. Awalannya hanya boleh huruf, underscore atau $
+//format : tipedata<spasi>namaVariabel
+//tipe data
+//int : bilangan bulat yang melakukan operasi aritmatika
+// float/double : bilangan desimal yang melakukan operasi aritmatika
+// string : huruf, simbol, kata, kalimat, angka(tidak melakukan operasi artimatika)
+// char : biasanya digunakan hanya dengan 1 karakter
+// boolean : hanya mengandung dua nilai saja yaitu true/false atau 1/0
+
+int umur, jumlahSaudara;
+float tiggiBadan, luas, lebar;
+String nama, alamat, npm;
+char huruf;
+boolean status; 
+//initialize
+//make something cool rn pal
+//buat wadah + kasih nilai awalnya
+int jumlahSigma = 35;
+jumlahSigma = 45;
+//const
+final float phi = 3.14f;
+//final itu nilai tidak boleh diubah
+//bloody hell mate, this shi' disgusting yk.
+//helloww how are you? i'm under the water, please help me it's too much rainy ooooo.....
+//jenis operator
+//tulis dikertas atau buku
+//jelaskan operator aritmatika dan penggunaannya dalam pemrograman java
+//1. operator aritmatika
+/*2. operator pembanding
+3. operator logika
+4. equals
+5. equalsIgnoreCase
+6. length
+7. charAt
+8. substring
+9. concat
+10. split
+11. toLowerCase, toUpperCase
+12. seleksi if
+13. seleksi if - else
+14. seleksi if - else - else if... else
+15. if - if - if - ....
+16. perulangan while
+17. perulangan do-while
+18. perulangan for
+SETIAP HALAMAN TULIS NAMA & NPM
+FOTO DENGAN JELAS DAN LENGKAP, JADIKAN 1 PDF KUMPUL KE SPON 
+SEBELUM HARI SELASA, 16 SEPTEMBER 2025, PUKUL 13.30 WIB
+*/
+    }
+    
+}
