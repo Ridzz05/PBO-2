c7725487e7890b4d9a199396f1bce4b0146f1299
M. Rizki A <muhrizkialghipari@gmail.com>
2025-09-22 08:33:53 +0700

    Update rizki_pertemuan3.java

--- rizki_pertemuan3.java
+++ rizki_pertemuan3.java
@@ -3,17 +3,15 @@ import java.util.Scanner;
 public class rizki_pertemuan3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        boolean ulang = true;
-        
-        while(ulang) {
+        boolean ipkValid = true;
+
+        while(ipkValid) {  // !not 
             // butuh variabel, untuk nampung berapa banyak data ipk yang diinput
             int banyak;
             System.out.print("Masukkan Banyak Data IPK = ");
             banyak = s.nextInt();
-            
             // Deklarasikan array ipk
             float[] ipk = new float[banyak];
-            
             // Input ipk
             for(int i = 0; i < banyak; i++) {
                 // latihan validasi input IPK kurang dari nol atau 
@@ -26,18 +24,26 @@ public class rizki_pertemuan3 {
                     }
                 } while(ipk[i] < 0 || ipk[i] > 4.00);
             }
-            
-            // Tampilkan isi array ipk
+
+            // tampilkan isi array ipk : ipk[0] + ipk[1] + ... = total 3.0 + 2.1 + 2.0 = 7.1
             System.out.println("\nData IPK yang diinput:");
+            float total = 0;
             for(int i = 0; i < banyak; i++) {
-                System.out.println("IPK ke-" + (i + 1) + " = " + ipk[i]);
+                System.out.print("IPK ke-" + (i + 1) + " = " + ipk[i]);
+                total += ipk[i];
+                if(i < banyak - 1) {
+                    System.out.print(" + ");
+                } else {
+                    System.out.print(" = " + total);
+                }
             }
+            System.out.println("\nTotal IPK = " + total);
             
             // Tanya apakah ingin mengulang program
             System.out.print("\nApakah Anda ingin mengulang program (Y/T)? ");
             String jawaban = s.next();
             if(!jawaban.equalsIgnoreCase("Y")) {
-                ulang = false;
+                ipkValid = false;
                 System.out.println("Program selesai");
             }
         }
